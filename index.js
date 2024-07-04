8463ad6f622bf57f26613fc493142257d51a88b7
Prachit Pandit <92202695+prachit082@users.noreply.github.com>
2024-07-04 19:35:27 +0530

    Added Comments

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ let colorTwo = document.getElementById("color-b");
 let currentDirection = 'to bottom';
 let outputCode = document.getElementById("code");
 
+/**
+ * Sets the direction value and updates the active button.
+ * @param {string} value - The direction value.
+ * @param {HTMLElement} _this - The button element.
+ */
 function setDirection(value, _this) {
     let direcrtions = document.querySelectorAll(".buttons button");
     for (let i of direcrtions) {
@@ -12,11 +17,17 @@ function setDirection(value, _this) {
     currentDirection = value;
 }
 
+/**
+ * Generates the CSS code for a linear gradient background and applies it to the body element.
+ */
 function generateCode() {
     outputCode.value = `background-image: linear-gradient(${currentDirection}, ${colorOne.value}, ${colorTwo.value})`;
     document.getElementsByTagName("BODY")[0].style.backgroundImage = `linear-gradient(${currentDirection}, ${colorOne.value}, ${colorTwo.value})`;
 }
 
+/**
+ * Copies the text from the outputCode element to the clipboard.
+ */
 function copyText() {
     outputCode.select();
     document.execCommand('copy');
